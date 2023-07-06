e7e5a39ac24991b4635ba3f74a49021d04e2f2fb
SaifAlqady <siafalqady51@gmail.com>
2023-07-06 14:05:12 +0300

    first commit

---
+++ src/styles/CompanyName.styles.ts
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+
+export const Name  = styled.h1`
+    color: white;
+`
---
+++ src/styles/Navbar.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.div`
+    background-color:black;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const LeftNavbar = styled.div`
+    width:10%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    
+`
+
+export const RightNavbar = styled.div`
+    width:88%;
+    display: flex;
+`
