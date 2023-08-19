219d98b67d82ee0d03a317b4558e1bd6ffd4bfab
SaifAlqady51 <saifalqady51@gmail.com>
2023-08-19 16:33:56 +0300

    deleting some unnecessary files

--- server/router.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import { getAirport } from './api';
-
-const router = Router();
-
-router.get('/airports', getAirport); // gets n airports from Amadeus API
-
-export default router;
