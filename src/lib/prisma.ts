f33c210200403ea995434546dee5cd38ee60a723
SaifAlqady51 <saifalqady51@gmail.com>
2023-10-21 11:33:56 +0300

    changing prisma client syntax

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,12 @@
 import { PrismaClient } from '../../prisma/generated/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
+
 declare global {
-    var prisma: any;
+	var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient().$extends(withAccelerate());
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient().$extends(withAccelerate());
-    }
-    prisma = global.prisma;
-}
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
