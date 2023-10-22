fa17ca717ecd9db58010eb6e66ea587d058b0759
SaifAlqady51 <saifalqady51@gmail.com>
2023-10-22 09:34:03 +0300

    changing apollo server http

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+
+
 const apolloClient = new ApolloClient({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: 'https://flight-booking-website-sandy.vercel.app/api/graphql',
     cache: new InMemoryCache(),
 });
 
