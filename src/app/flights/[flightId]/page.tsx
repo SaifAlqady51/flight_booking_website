1299bb57379a7f06c2f4d4c6e038fc3821293a34
SaifAlqady51 <saifalqady51@gmail.com>
2023-11-03 07:26:33 +0200

    changing component props Type

--- src/app/flights/[flightId]/page.tsx
+++ src/app/flights/[flightId]/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { FlightStopsLine } from '@/app/components/Flights/FlightStopsLine';
+import {FlightType} from '@/types/flight-types';
 
-const Page = ({flightData}:{flightData:any}) => {
+const Page = ({flightData}:{flightData:FlightType}) => {
     return <FlightStopsLine  flightData={flightData}/>;
 };
 
