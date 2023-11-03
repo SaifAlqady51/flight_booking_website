9d9c4d5e5b1bf6b17c4743a1e882033b6ae39e9f
SaifAlqady51 <saifalqady51@gmail.com>
2023-11-03 07:29:08 +0200

    commenting flightId page

--- src/app/flights/[flightId]/page.tsx
+++ src/app/flights/[flightId]/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { FlightStopsLine } from '@/app/components/Flights/FlightStopsLine';
-import {FlightType} from '@/types/flight-types';
 
-const Page = ({flightData}:{flightData:FlightType}) => {
-    return <FlightStopsLine  flightData={flightData}/>;
+const Page = () => {
+    return (
+		<div>Flight id</div>
+	);
 };
 
 export default Page;
