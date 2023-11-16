2a8c3ca48b35788e16ccb370e56be2bdfe372667
SaifAlqady51 <saifalqady51@gmail.com>
2023-11-16 19:34:19 +0200

    adding routes to payment method

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     },
     env: {
         DATABASE_URL: process.env.DATABASE_URL,
-        AIRLABS_API_KEY: process.env.AIRLABS_API_KEY,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         URL: process.env.URL,
--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -15,8 +15,8 @@ export async function POST(request: NextRequest) {
                 },
             ],
             mode: 'subscription',
-			success_url: 'http://localhost:3000/payment',
-			cancel_url: 'http://localhost:3000/pricing'
+			success_url: `${process.env.URL}payment`,
+			cancel_url: `${process.env.URL}pricing`
         });
 
 		console.log(session.url)
