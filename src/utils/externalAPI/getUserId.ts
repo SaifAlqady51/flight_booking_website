38792748c0467159b67edd58c6b019e46e4fe76a
SaifAlqady51 <saifalqady51@gmail.com>
2023-11-21 08:33:39 +0200

    changing the api url

--- src/utils/externalAPI/getUserId.ts
+++ src/utils/externalAPI/getUserId.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getUserId = async () => {
     try {
-        const response = await axios.get('http://localhost:3000/api');
+        const response = await axios.get(`${process.env.URL}/api`);
         return response.data.id;
     } catch (error) {
         throw new Error(`Error : ${error}`);
