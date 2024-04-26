139c1fc527a85cb7df3c854b99b92090b8553ee0
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-26 22:54:13 +0300

    ui: Added alert for noSignedIn condition

---
+++ src/styles/PricingStyles/NotSignedInAlert.styles.ts
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const NotSignedInAlert = styled.div`
+    position: absolute;
+    bottom: 5px;
+    color: #a4133c;
+    font-weight: 500;
+    border-radius: 10px;
+`;
